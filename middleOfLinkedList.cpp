68a5d7f7e6f5db768d00a21d4e4aad1ac64b3286
dev082000 <devanshsingh.chandel@gmail.com>
2024-03-24 06:28:43 -0700

    Middle of linked List Problem
    Given the head of a singly linked list, return the middle node of the linked list.
    
    If there are two middle nodes, return the second middle node.

---
+++ middleOfLinkedList.cpp
@@ -0,0 +1,36 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        if (head == NULL)
+        {
+            return 0;
+        }
+        if (head->next == NULL)
+        {
+            return head;
+        }
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast != NULL)
+        {
+            fast = fast->next;
+            if (fast != NULL)
+            {
+                fast = fast->next;
+                slow = slow->next;
+            }
+        }
+        return slow;
+    }
+};
