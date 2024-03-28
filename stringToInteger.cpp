e364ff7534ec1dc8baa711584bc8ad2064c0e9be
dev082000 <devanshsingh.chandel@gmail.com>
2024-03-28 22:54:42 +0530

    String To Integer
    Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).

---
+++ stringToInteger.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+     
+    
+        int i=0;
+        int sign=1;
+        long ans=0;
+        while(i<s.length() && s[i]==' ')
+            i++;
+        if(s[i]=='-')
+         {
+            sign=-1;
+            i++;
+        }
+        else if(s[i]=='+')
+            i++;
+        while(i<s.length())
+        {
+            if(s[i]>='0' && s[i]<='9')
+…        }
+        return (ans*sign); }
+    
+};
